b4161a8c73b7426afeeb7baad6f923e0d0a9cd06
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-07 13:47:46 -0500

    more basic operations with py

--- inputProg.py
+++ inputProg.py
@@ -1,4 +1,22 @@
-greetings = "Greetings"
-who = "Earthlings"
-whoYouR = input("Please enter your cover name for this assignment ")
-print(greetings + ' Agent ' + whoYouR)
+# greetings = "Greetings"
+# who = "Earthlings"
+# whoYouR = input("Please enter your cover name for this assignment ")
+# print(greetings + ' Agent ' + whoYouR)
+
+splitString = "This string has been\nsplit over\nseveral lines."
+print(splitString)
+
+tabbedString = "123\t234\t298\t9843\t098423"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\s resting"')
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\"")
+
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+print(anotherSplitString)
+
+print('''The pet shop owner said "No, no, 'e's uh,...he's resting"''')
+print("""The pet shop owner said "No, no, 'e's' uh,...he's resting" """)
---
+++ variables.py
@@ -0,0 +1,11 @@
+greeting = "Bruce"
+_myName = "Chango"
+Raiden777 = "Bra"
+Raiden_Var_999 = "Morhjens"
+Moro = "Sup"
+print(Raiden_Var_999 + ' ' + greeting)
+
+age = 777
+print(age)
+
+print(greeting + str(age))
