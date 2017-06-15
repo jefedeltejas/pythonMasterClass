55073f9e4dc1d663fc8a1195234fc75b8b3c3354
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-15 09:53:09 -0500

    added some elif and else flow control

--- ifprogramflow.py
+++ ifprogramflow.py
@@ -1,3 +1,11 @@
 name = input("Please enter your name: ")
+age = int(input("How old are you, {0}?".format(name)))
 color = input("What's your favorite color, {0}?".format(name))
 print(color)
+
+if age >= 189:
+    print("You're very old homie")
+elif age >= 16:
+    print("Time to get a job")
+else:
+    print("Sorry you are not eligible for our services. Please come back in {0} years.".format(18 -age))
