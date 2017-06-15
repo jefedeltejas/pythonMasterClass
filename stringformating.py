bd2247b1ae3a062fc64cc2687a2cc21e9bf782d6
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-15 09:35:21 -0500

    more formating now with flow control

--- stringformating.py
+++ stringformating.py
@@ -29,3 +29,11 @@ print("Pi is approximately %12.50f" % ( 22 / 7))
 
 x = sum(range(1, 9))
 print(x)
+
+for i in range(1, 12):
+    print("No. {0:2} squared is {1:<4} and cubed is {2:<4}".format(i, i ** 2, i ** 3))
+
+print("Pi is approximately {0:12.50}".format( 22 / 7))
+
+for i in range(1, 12):
+    print("No. {} squared is {} and cubed is {:<4}".format(i, i ** 2, i ** 3))
