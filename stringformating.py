a3243510b9de781f4cfbbf0d5da448c9b4ffa99b
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-15 08:14:57 -0500

    added more format examples

--- stringformating.py
+++ stringformating.py
@@ -5,3 +5,27 @@ print("My age is {0} years".format(age))
 
 print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7} ".format(31, "January",
     "March", "May", "July", "August", "October", "December"))
+
+print("""January: {2}
+February: {0}
+March: {2}
+April: {1}
+May: {2}
+June: {1}
+July: {2}
+August: {2}
+September: {1}
+October: {2}
+November: {1}
+December: {2}""".format(28, 30, 31),'\n')
+
+print("My age is %d years" % age)
+print("My age is %d %s, %d %s" % (age, "years", 6, "months"))
+
+for i in range(1, 12):
+    print("No. %2d squared is %4d and cubed is %4d" %(i, i ** 2, i ** 3))
+
+print("Pi is approximately %12.50f" % ( 22 / 7))
+
+x = sum(range(1, 9))
+print(x)
